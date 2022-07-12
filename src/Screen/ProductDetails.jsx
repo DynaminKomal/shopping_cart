efc71c489703b8dd0ba2d9549cf3ed72baae7677
Komal <komalparajapati167@gmail.com>
2022-07-12 13:12:40 +0530

    added upto some change

--- src/Screen/ProductDetails.jsx
+++ src/Screen/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams, Link, Navigate } from 'react-router-dom';
+import { useParams, Link, Navigate, useNavigate } from 'react-router-dom';
 import { Button, Col, Container, Form, Image, ListGroup, ListGroupItem, Row } from 'react-bootstrap'
 import Rating from './Rating';
 import { listProductDetails } from '../Actions/productAction'
@@ -9,7 +9,7 @@ import MessageBox from "../Components/MessageBox";
 
 function ProductDetails() {
 
-
+    const navigate = useNavigate();
     const [qty, setQty] = useState(1);
    
     const { id } = useParams();
@@ -17,12 +17,19 @@ function ProductDetails() {
     const productDetails = useSelector((state) => state.productDetails);
     const { loading, error, product } = productDetails;
 
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+
     useEffect(() => {
         dispatch(listProductDetails(id))
     }, [dispatch, id])
 
     const handleAddToCart = ()=>{
-        Navigate(`/cart/${id}?qty=${qty}`,qty);
+        if(userInfo){
+            navigate(`/cart/${id}?qty=${qty}`, qty);
+        }else{
+            navigate("/login");
+        }
     }
 
     return (
